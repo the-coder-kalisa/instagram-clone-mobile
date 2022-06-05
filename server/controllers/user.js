7fce64a9fa9f4c593d5c8556f95de293e541ed4e
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-06-05 18:10:15 +0200

    added backend

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,7 +1,11 @@
+const  joi = require('joi');
 exports.signup = async(req, res) =>{
-    console.log(req.body)
-    const {} = req.body;
+    const {email, fullname, username, password} = req.body;
     try {
+        const {error} =  schema.validate(req.body);
+        if(error){
+            return res.status(400).send(error.details[0].message);
+        }
         
     } catch (error) {
         console.log(error)
@@ -10,3 +14,9 @@ exports.signup = async(req, res) =>{
 
 exports.login = async(req, res) =>{
 }
+const schema = joi.object().keys({
+    email: joi.string().email().required(),
+    fullname: joi.string().min(5).required(),
+    username: joi.string().max(10).required(),
+    password: joi.string().min(8).regex(/^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z])$/)
+}) 
